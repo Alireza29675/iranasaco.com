ae9ad374468fe8065d1a1d4e661ccad8ccee3483
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2017-08-26 16:45:04 +0430

    Every part connected

--- src/scripts/Robot.js
+++ src/scripts/Robot.js
@@ -1,8 +1,10 @@
 const loader = new THREE.JSONLoader()
 
-const blackMaterial = new THREE.MeshLambertMaterial({ color: 0x111111 })
-const darkMaterial = new THREE.MeshLambertMaterial({ color: 0x999999 })
-const lightMaterial = new THREE.MeshLambertMaterial({ color: 0xeeeeee })
+const blackMaterial = new THREE.MeshPhongMaterial({ color: 0x000000, shininess: 0 })
+const darkMaterial = new THREE.MeshPhongMaterial({ color: 0x999999, shininess: 100 })
+const lightMaterial = new THREE.MeshPhongMaterial({ color: 0xeeeeee, shininess: 70 })
+
+let time = 0;
 
 const objectsNames = ['arm1', 'arm2', 'finger1', 'finger2', 'hand', 'rest', 'rotator', 'wrist', 'wristbone']
 const load = (objectName) => {
@@ -33,7 +35,7 @@ class Robot {
     constructor (roboScene) {
         this.roboScene = roboScene
         this.scene = roboScene.scene
-        this.mesh = new THREE.Object3D()
+        this.isReady = false
         this.parts = {}
         loadAllObjectsAndPutIn(this.parts).then(this.ready.bind(this))
     }
@@ -42,13 +44,31 @@ class Robot {
             const part = this.parts[partName]
             part.castShadow = true
             part.receiveShadow = true
-            this.mesh.add(part)
         }
-        this.mesh.position.z = 0
-        this.scene.add(this.mesh)
+        // Combining things together
+        this.parts.rest.add(this.parts.rotator)
+        this.parts.rotator.add(this.parts.arm1)
+        this.parts.arm1.add(this.parts.arm2)
+        this.parts.arm2.add(this.parts.wrist)
+        this.parts.wrist.add(this.parts.wristbone)
+        this.parts.wristbone.add(this.parts.hand)
+        this.parts.hand.add(this.parts.finger1)
+        this.parts.hand.add(this.parts.finger2)
+        // adding rest to scene
+        this.scene.add(this.parts.rest)
+        this.isReady = true
+    }
+    set rotateY (deg) {
+        this.parts.rotator.rotation.y = deg
+    }
+    get rotateY () {
+        return this.parts.rotator.rotation.y
     }
     render () {
-        // this.mesh.rotation.y += 0.001
+        time++
+        if (this.isReady) {
+            this.rotateY += 0.01
+        }
     }
 }
 
