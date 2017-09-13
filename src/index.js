4ee5ab352165568bdad38d2bbca3dd8eaad8e0b2
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2017-09-14 01:30:18 +0430

    loader removes after hiding to increase memory

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import './stylesheets/index.sass'
 import App from './scripts/App'
 
-window.onload = function () {
-    document.body.classList.add('loading-finished')
-};
-
 window.$ = (query) => document.querySelector(query);
 window.$$ = (query) => document.querySelectorAll(query);
+
+window.onload = function () {
+    $('body').classList.add('loading-finished');
+    setTimeout(() => { $('body > .loading').remove() }, 1000)
+};
 
 window.app = new App;
