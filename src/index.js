14c30827fae06d91a615ae49eba8eecce4abae7d
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2017-09-14 01:15:30 +0430

    Loading works fine

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import './stylesheets/index.sass'
 import App from './scripts/App'
 
-window.$ = (query) => document.querySelector(query)
-window.$$ = (query) => document.querySelectorAll(query)
+window.onload = function () {
+    document.body.classList.add('loading-finished')
+};
 
-window.app = new App
+window.$ = (query) => document.querySelector(query);
+window.$$ = (query) => document.querySelectorAll(query);
+
+window.app = new App;
